c8614bca01f00d128c6e701a2a4dfa2ccd200ae3
feride <ozdemirfd@gmail.com>
2020-12-27 17:20:11 -0600

    Changing in App.js

--- src/App.js
+++ src/App.js
@@ -23,27 +23,7 @@ function App() {
     <Button title="Click me!!" click={buttonClicked}/>
     </div>
   )
-  //(
-
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-
-    
- // );
+ 
 }
 
 
